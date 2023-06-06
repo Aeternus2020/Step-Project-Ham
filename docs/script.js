67c47164f52ae61c0170c9cf937eda5d2914b846
Olena Hodyna <crimeaodessa2006@gmail.com>
2023-06-06 17:15:10 +0200

    change section work(random images)

--- docs/script.js
+++ docs/script.js
@@ -9,16 +9,20 @@ const work = document.querySelector(".work");
 const left = document.getElementById('left');
 const right = document.getElementById('right');
 const commt = document.querySelector('.list_commet');
-const morework = document.getElementById('moreWork');
 const tabscont = document.querySelector('.tabs-contents');
 const major = document.querySelector('.major');
 const hover = document.getElementById('hover');
 const imgsWork = document.querySelectorAll('.work_tabs img');
 const allWork = document.querySelector('.all');
+const workWrapper = document.querySelector('.work_wrapper');
 let second;
 let ava;
 
-
+const buttonAdd = document.createElement("button");
+      buttonAdd.id = "moreWork";
+      buttonAdd.innerHTML = "<span>&#43;</span>Load More";
+      workWrapper.appendChild(buttonAdd);
+const morework = document.getElementById('moreWork');
 
 //Создание списков комментаторов
 const arr = [`<p class="text">The most impressive was their devotion to making sure our needs were met. They very carefully recognized our needs and understood the way our company and customer service function. It was a great decision to start working with them!</p>
@@ -37,8 +41,9 @@ const arr = [`<p class="text">The most impressive was their devotion to making s
             <h3 class="name">Arianna Huffington</h3>
             <p class="profession">Talent Researcher</p>
             <img src="./Pictures/testimonials/3.png" alt="Talent Researcher"/>`];
-  const ul = document.createElement("ul");
-  ul.className = "list_commentators";
+
+const ul = document.createElement("ul");
+ul.className = "list_commentators";
 
 function creat() {
   arr.forEach((element) => {
@@ -59,139 +64,121 @@ function creat() {
 creat(arr);
 
 //Скроллинг комментаторов влево
+right.addEventListener('click', () => {
+  scroll(rightScroll, "-30px")
+})  
+
+//Скроллинг комментаторов вправо
 left.addEventListener('click', () => {
+  scroll(leftScroll, "30px")
+})  
+
+//Скроллинг комментаторов 
+function scroll(side, y) {
   second = document.querySelector('.second');
   second.animate([
     {transform: 'translate(0, 0)'},
     {transform: 'translate(0, 10px)'},
-  ], 800);
-  ava.animate([
-    {transform: 'translate(0, 0)', opacity: 1},
-    {transform: 'translate(0, 0)',opacity: 0}
   ], 1000);
+    ul.animate([
+      {transform: 'translate(0, 0)', opacity: 1},
+      {transform: `translate(${y}, 0)`, opacity: 0}
+    ], 1000);
+    setTimeout(side, 800);
+    setTimeout(animRev, 800);
+}
+
+//Анимация аватара
+function animRev() {
+    ava.animate([
+      {transform: 'translate(0, 0)', opacity: 1},
+      {transform: 'translate(0, 0)',opacity: 0}
+    ], 1000);
+    ava.animate([
+        {transform: 'translate(0, 0)', opacity: 0},
+        {transform: 'translate(0, 0)',opacity: 1}
+      ], 1000)
+}
+
+//Анимация скролла
+function scrollUl(y) {
   ul.animate([
-    {transform: 'translate(0, 0)', opacity: 1},
-    {transform: 'translate(-30px, 0)', opacity: 0}
+    {transform: `translate(${y}, 0)`, opacity: 0},
+    {transform: 'translate(0, 0)', opacity: 1}
   ], 1000);
-  setTimeout(leftScroll, 800);
-  setTimeout(animRev, 800);
-})  
+  second.animate([
+    {transform: 'translate(0, 10px)'},
+    {transform: 'translate(0, 0)'},
+  ], 1000);
+}
 
 //Анимация скролла влево
-function leftScroll() {
+function rightScroll() {
   let elm = arr.shift(0);
   arr.push(elm);
   ul.innerHTML = '';
   major.innerHTML = '';
   creat(arr);
   second = document.querySelector('.second');
-  ul.animate([
-    {transform: 'translate(30px, 0)', opacity: 0},
-    {transform: 'translate(0, 0)', opacity: 1}
-  ], 800);
-  second.animate([
-    {transform: 'translate(0, 10px)'},
-    {transform: 'translate(0, 0)'},
-  ], 800);
-}
-
-//Скроллинг комментаторов вправо
-right.addEventListener('click', () => {
-  second = document.querySelector('.second');
-  second.animate([
-    {transform: 'translate(0, 0)'},
-    {transform: 'translate(0, 10px)'},
-  ], 800);
-  ava.animate([
-    {transform: 'translate(0, 0)', opacity: 1},
-    {transform: 'translate(0, 0)',opacity: 0}
-  ], 800);
-    ul.animate([
-      {transform: 'translate(0, 0)', opacity: 1},
-      {transform: 'translate(30px, 0)', opacity: 0}
-    ], 800);
-    setTimeout(rightScroll, 800);
-    setTimeout(animRev, 800);
-})  
-
-//Анимация аватара
-function animRev() {
-  ava.animate([
-        {transform: 'translate(0, 0)', opacity: 0},
-        {transform: 'translate(0, 0)',opacity: 1}
-      ], 800)
+  scrollUl("30px");
 }
 
 //Анимация скролла вправо
-function rightScroll() {
+function leftScroll() {
   let elm = arr.pop(3);
   arr.unshift(elm);
-  ul.textContent = '';
+  ul.innerHTML = '';
   major.innerHTML = '';
   creat(arr);
   second = document.querySelector('.second');
-  ul.animate([
-    {transform: 'translate(-30px, 0)', opacity: 0},
-    {transform: 'translate(0, 0)', opacity: 1}
-  ], 800);
-  second.animate([
-    {transform: 'translate(0, 10px)'},
-    {transform: 'translate(0, 0)'},
-  ], 800);
+  scrollUl("-30px");
 }
 
 //Переключение вкладки по клику
+function toggle(target, buttonList, list) {
+  buttonList.forEach((button, index) => {
+    button.dataset.status = (button === target) ? "active" : "inactive";
+    list[index].dataset.status = button.dataset.status;
+  });
+  worklist[0].dataset.status !== "active" || allWork.children.length === 24 ?  morework.remove() : workWrapper.appendChild(buttonAdd);
+}
+
 work.addEventListener('click', (event) => {
+  let newArrClone =[]
+  arrClone.forEach(img => {
+    if (img.alt === event.target.innerHTML) {
+      newArrClone.push(img);
+      img.style.display = "block";
+    }
+  })
   toggle(event.target, worklist, worktabs);
-  morework.style.display === 'none' ? morework.style.display = 'block' : null
 });
 
 tabs.addEventListener("click", (event) => {
   toggle(event.target, tabttl, tabmain);
 });
 
-function toggle(target, buttonList, list) {
-  buttonList.forEach((button, index) => {
-    button.dataset.status = (button === target) ? "active" : "inactive";
-    list[index].dataset.status = button.dataset.status;
-  });
-}
+const arrClone = [];
 
-
-let arrClone = [];
-
-//Рандомные картинки в All
+//Добавление картинок
 function showAllImg(length) {
-  for (let i = 0; i < 12; i++) {
-  if (allWork.children.length < length) {
-      let rand = Math.floor(Math.random() * 94);
-      if (arrClone.includes(rand)) {
-        return showAllImg(length);
-      }
-        arrClone.push(rand);
-        let clone = imgsWork[rand].cloneNode();
-        clone.style.display = 'block';
-        allWork.append(clone);
+  while (allWork.children.length < length) {
+    let rand = Math.floor(Math.random() * 94);
+    let selectedImg = imgsWork[rand];
+    
+    if (!arrClone.includes(selectedImg)) {
+      let clone = selectedImg.cloneNode();
+      clone.style.display = 'block';
+      allWork.append(clone);
+      arrClone.push(selectedImg);
     }
   }
+  morework.addEventListener('click', () => {
+    for (let i = 0; i < worklist.length; i++) {
+      worklist[0].dataset.status === "active" ?  showAllImg(24) : null;
+      }
+      morework.remove();
+    });
 }
 
 showAllImg(12);
-let sss = 0;
-
-//Добавление картинок
-morework.addEventListener('click', () => {
-  for (let i = 0; i < worklist.length; i++) {
-    if (worklist[0].dataset.status === "active") {
-      showAllImg(24);
-    }
-    if (worklist[i].dataset.status === "active") {
-        for (let y = 0; y < 24; y++) {
-          if (worktabs[i].children[y].style.display === 'none') {
-            worktabs[i].children[y].style.display = 'block';
-          } 
-        }
-        morework.style.display = 'none';
-    }
-  }
-});
